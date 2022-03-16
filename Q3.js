dc264a2ed12922095cc776a78f3f385fd5dee471
tuyizerejean <tuyibobu@gmail.com>
2022-03-16 10:11:48 +0200

    Adding the screenshoot

--- Q3.js
+++ Q3.js
@@ -5,7 +5,7 @@ console.log("Index of France element is:",francIndex)
 const deleteIndex=countries.splice(1,francIndex)
 console.log("Elements of the array after removing France:" ,countries)
 // Working on the array to accept countries that start with 'E'
-console.log("---Working on the array to accept countries that start with 'E'-----")
+console.log("----Working on the array to accept countries that start with 'E'-----")
 let eCountries = []
 countries.forEach((item) => {
   	if(item.startsWith('E')){
